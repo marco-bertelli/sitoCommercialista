1c5b6853b82461d6a9f8996aaa88a242acdb2bc2
unknown <marcobert37@gmail.com>
2020-08-30 14:28:40 +0200

    carosello finito

--- sitoCommercialista/src/app/app.module.ts
+++ sitoCommercialista/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import { MDBBootstrapModule, CarouselModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    CarouselModule.forRoot(),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
