1d0998fa745408c0550b3cd36ff3d5a66e0c327b
unknown <marcobert37@gmail.com>
2020-08-30 13:43:42 +0200

    installazione libreria figa per carosello

--- sitoCommercialista/src/app/app-routing.module.ts
+++ sitoCommercialista/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
