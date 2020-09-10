d6145a7e99e648d0f048382f3cd4dda03ab22c97
contilorenzo <lorenzo.conti.bg@gmail.com>
2020-09-10 17:27:11 +0200

    aggiunto html di area clienti, non va il routerlink

--- sitoCommercialista/src/app/features/areaclienti/areaclienti.module.ts
+++ sitoCommercialista/src/app/features/areaclienti/areaclienti.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { AreaclientiRoutingModule } from './areaclienti-routing.module';
 import { AreaclientiComponent } from './areaclienti.component';
+import { MenuModule } from '../menu/menu.module';
+import { FooterModule } from '../footer/footer.module';
 
 
 @NgModule({
   declarations: [AreaclientiComponent],
   imports: [
     CommonModule,
-    AreaclientiRoutingModule
+    AreaclientiRoutingModule,
+    MenuModule,
+    FooterModule
   ]
 })
 export class AreaclientiModule { }
